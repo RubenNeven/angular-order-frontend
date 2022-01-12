f9d5ef2ae9f545d31ceb8488a280755474f60b67
RubenNeven <rubenneven@gmail.com>
2022-01-12 14:12:39 +0100

    Story IT3: Create Item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {ItemGalleryComponent} from './item-gallery/item-gallery.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FilterPipe } from './filter.pipe';
+import { CreateItemComponent } from './create-item/create-item.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemGalleryComponent,
+    FilterPipe,
+    CreateItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/item.ts
@@ -0,0 +1,8 @@
+export interface Item {
+  name: string;
+  price: number;
+  image: string;
+  stockUrgency: string;
+  amountOfStock: number;
+
+}
